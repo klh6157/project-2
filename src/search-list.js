439004c97ef9e4229071c54d716fdda3e2b34724
klh6157 <Scytrius@gmail.com>
2023-04-17 18:11:07 -0400

    commit

--- src/search-list.js
+++ src/search-list.js
@@ -29,75 +29,7 @@ export class Project2 extends LitElement {
     return {
       badges: { type: Array },
     };
-  }
-
-  // static styles = css`
-  //   :host {
-  //     display: block;
-  //     color: #1a2b42;
-  //     background-color: var(--project-2-background-color);
-  //   }
-  //   .titles {
-  //     flex-direction: row;
-  //     position: absolute;
-  //     font-size: 20px;
-  //     top: 8%;
-  //     left: 5%;
-  //     margin-top: 10px;
-  //     margin-bottom: 10px;
-  //   }
-
-  //   @keyframes app-logo-spin {
-  //     from {
-  //       transform: rotate(0deg);
-  //     }
-  //     to {
-  //       transform: rotate(360deg);
-  //     }
-  //   }
-
-  //   .app-footer {
-  //     font-size: calc(12px + 0.5vmin);
-  //     align-items: center;
-  //   }
-
-  //   .app-footer a {
-  //     margin-left: 5px;
-  //   }
-
-  //   .object {
-  //     top: 5%;
-  //     left: 5%;
-  //     width: 90%;
-  //     height: 10%;
-  //     background-color: #91cdff8a;
-  //     border-left: 15px solid #00a2ff;
-  //     border-radius: 5px;
-  //     cursor: pointer;
-  //     outline: none;
-  //     text-align: left;
-  //     font-size: 15px;
-  //     border-top:none;
-  //     border-right:none;
-  //     border-bottom:none;
-  //     display: block;
-      
-  //   }
-  //   .search {
-  //     width: 900px;
-  //     height: 32px;
-  //     border: 1px solid #f8f8f7;
-  //     display: block;
-  //     font-family: var(--badge-app-font-family);
-  //     padding: 8px 4px 8px 64px;
-  //     box-shadow: 1px 1px 5px 0.5px #bbb6b0;
-  //     background: transparent url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath fill='%239a9691' d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E") no-repeat 13px center;
-  //     background-position: 24px;
-  //   }
-  //   .search:focus{
-  //     outline: none;
-  //     background-color: #f8f8f7f2;
-  //   }
+  }   
 
   constructor() {
     super();
